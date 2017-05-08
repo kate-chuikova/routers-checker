054e55492e4d742f0c40b02fbc0b96ecd9a383e2
shulg95 <shulg95@mail.ru>
2017-05-08 03:24:11 +0300

    [master-validators] Port and Host names validators added

---
+++ src/main/java/com/routers/utils/validators/Validators.java
@@ -0,0 +1,7 @@
+package com.routers.utils.validators;
+
+public class Validators {
+    public static final HostValidator hostValidator = new HostValidator();
+    public static final PortValidator portValidator = new PortValidator();
+}
+
